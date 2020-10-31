701db0ef013a3178d32d43056cfe9f9f712ea903
Anessivan <Anessivan@mail.ru>
2020-10-31 16:33:13 +0300

    first commit

---
+++ res/Main.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+
+#include "Methods.h"
+
+
+
+void Print(double* data, int length)
+{
+  for (int i = 0; i < length; i++)
+    std::cout << std::endl << data[i];
+}
+
+void FPrint(double* data, int length, std::ofstream& out, double power, double left, double right, double dx)
+{
+  if (out.is_open())
+  {
+    out << " (" << left << "; " << right << ")  power = " << power << "  dx = " << dx;
+    for (int i = 0; i < length; i++)
+      out << std::endl << data[i];
+  }
+  out.close();
+}
+
+double* Error(double* methoddata, int length, double power, double left, double right, double dx)
+{
+  double* ErrorData = new double[length];
+  for (int i = 0; i < length; i++)
+    ErrorData[i] = abs(exp(power *(left + i * dx)) - methoddata[i]);
+  return ErrorData;
+}
+
+int main()
+{
+  double left = 0;
+  double right = 2;
+  double power = 2;
+  double y0 = 1;
+  double dx = 0.01;
+  double* EulerData = nullptr;
+  double* RungeKuttaData = nullptr;
+  EulerData = EulerMethod(left, right, y0, power, dx);
+  RungeKuttaData = RungeKutta(left, right, y0, power, dx);
+  int length = floor((abs(right - left)) / dx) + 1;
+  double* ErrorEuler = nullptr;
+  ErrorEuler = Error(EulerData,length, power, left, right, dx);
+  double* ErrorRungeKutta = nullptr;
+  ErrorRungeKutta = Error(RungeKuttaData, length, power, left, right, dx);
+  std::cout << "Euler Method";
+  Print(EulerData, length);
+  std::cout << "Runge-Kutta Method";
+  Print(RungeKuttaData, length);
+  std::ofstream out;
+  out.open("..//Euler.txt");
+  FPrint(EulerData, length, out, power, left, right, dx);
+  out.close();
+  out.open("..//Runge-Kutta.txt");
+  FPrint(RungeKuttaData, length, out, power, left, right, dx);
+  out.close();
+  out.open("..//ErrorEuler.txt");
+  FPrint(ErrorEuler, length, out, power, left, right, dx);
+  out.close();
+  out.open("..//ErrorRungeKutta.txt");
+  FPrint(ErrorRungeKutta, length, out, power, left, right, dx);
+  out.close();
+  delete[] ErrorRungeKutta;
+  delete[] ErrorEuler;
+  delete[] EulerData;
+  delete[] RungeKuttaData;
+}
---
+++ res/Methods.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+
+
+#include <iostream>
+#include <cmath>
+
+double* EulerMethod(const double left, const double right, const double y0, const double power, const double dx)
+{
+  int length = floor((abs(right - left)) / dx) + 1;  // Вычисление количества элементов массива
+  double* data = nullptr;
+  data = new double[length];
+  data[0] = y0;
+  for (int i = 1; i < length; i++)
+  {
+    data[i] = data[i - 1] + data[i - 1] * power * dx * dx * i; // x(t) * (1 + x'(t)*eps)
+  }
+  return data;
+}
+
+double* RungeKutta(const double left, const double right, const double y0, const double power, const double dx)
+{
+  int length = floor((abs(right - left)) / dx) + 1;
+  double* data = new double[length];
+  double k1 = y0, k2 = y0, k3 = y0, k4 = y0;
+  data[0] = y0;
+  double x = y0;
+  int i = 0;
+  for (int i = 0; i < length - 1; i++)
+  {
+    k1 = power * data[i];
+    k2 = power *(data[i] + dx * k1 / 2.0 );
+    k3 = power * (data[i] + dx * k2 / 2.0 );
+    k4 = power * (data[i] + dx * k3);
+    data[i + 1] = data[i] + (dx / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4);
+  }
+  return data;
+}
