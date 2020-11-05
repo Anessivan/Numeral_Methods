07ce98e523c259e6b9a96a03fdbea60f057ca8c5
Anessivan <Anessivan@mail.ru>
2020-11-05 20:19:21 +0300

    add Cmake

---
+++ Build/CMakeFiles/Methods.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anessivan/Drives/700Gb/GitProjects/Numeral_Methods/res/Methods.cpp" "/home/anessivan/Drives/700Gb/GitProjects/Numeral_Methods/Build/CMakeFiles/Methods.dir/Methods.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/CMakeFiles/Methods.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libMethods.a"
+)
